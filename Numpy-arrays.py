a27bf7eadf497c136ef99382eaf101100b5e98fb
Bernard-564 <bernardalter96@gmail.com>
2025-11-16 09:52:51 +0100

    Add PyCharm project files and update numpy array code
    Added .idea configuration files for PyCharm project setup. Updated Numpy-arrays.py to demonstrate reshaping a 2D array into a 3D matrix and printing its shape, replacing previous examples and commented-out code.

--- Numpy-arrays.py
+++ Numpy-arrays.py
@@ -5,13 +5,15 @@ Array1 = np.arange(1,16)
 Array2 = np.arange(1,16,2)
 Array3 = np.arange(16).reshape(4,4)
 
-
-print(Array)
+"""print(Array)
 print(Array1)
 print(Array2)
-print(Array3)
+print(Array3)"""
 
 # arrays from lists
-from_list = np.array([1,2,3,4,5])
-print(type(from_list[0]))
-print(from_list)
+from_list = np.array([[1,3,5,7],[0,2,4,6]])
+D2matrix = (from_list.reshape(2,2,2))
+#print(type(from_list[0]))
+#print(from_list)
+print(D2matrix)
+print(D2matrix.shape)
