3d4ae1680f84731a93ef8ee8fb79afc952c33dda
Bernard-564 <bernardalter96@gmail.com>
2025-12-02 06:05:51 +0100

    Add price and inflation logic to Book class
    Introduced price attribute and inflation calculation to the Book class. Added a method to apply inflation based on book age, updated constructor parameters, and adjusted example usage accordingly.

--- Python.OOP/Classes.py
+++ Python.OOP/Classes.py
@@ -1,17 +1,32 @@
+from datetime import date
+
+
 class Book:
-    def __init__ (self, title, author, year):
+    inflation_rate = 1.03  
+    current_year = date.today().year
+
+    def __init__ (self, title, author, Ryear, price):
         self.title = title
         self.author = author
-        self.year = year
+        self.Ryear = Ryear
+        self.price = price
         self.email = author + title + "@Ibookstore.com"
 
     def honors(self):
-        return f"{self.title} by {self.author} ({self.year})"
+        return f"{self.title} by {self.author} ({self.Ryear})"
+    
+    def aplly_inflation(self):
+         age = Book.current_year - self.Ryear
+         times_applied = age // 5
+         self.price = round(self.price * (Book.inflation_rate ** times_applied), 2)
 
 
-volume1 = Book("Greed", "Marino", 2025)
-volume2 = Book("The Last Kingdom", "Cornwell", 2004)
+volume1 = Book("Greed", "Marino", 2025, 17.99)
+volume2 = Book("The Last Kingdom", "Cornwell", 2004, 21.68)
 
-print(volume1.honors())
-#print(volume2.honors())
-print(Book.honors(volume2))  # Alternative way to call the method
+'''print(volume1.honors())
+print(volume2.honors())
+print(Book.honors(volume2))  # Alternative way to call the method'''
+
+volume2.aplly_inflation()
+print (volume2.price)
